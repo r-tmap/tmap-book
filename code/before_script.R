98a272c9d90bae25b763d88cb011cc557be82211
Jakub Nowosad <tupiszakaczki@gmail.com>
2021-09-05 13:09:51 +0200

    tries a new approach of interactive maps in both html and pdf with the view_map function

--- code/before_script.R
+++ code/before_script.R
@@ -1,21 +1,23 @@
 library(methods)
+library(webshot)
 # knitr::knit_hooks$set(crop = knitr::hook_pdfcrop)
 knitr::opts_chunk$set(
   background = "#FCFCFC", # code chunk color in latex
   comment = "#>",
   collapse = TRUE,
   cache = TRUE, #https://github.com/rstudio/bookdown/issues/15#issuecomment-591478143
-  fig.pos = "h", #"t"
+  # fig.pos = "h", #"t"
   fig.path = "figures/",
   fig.align = "center",
-  fig.height = 7,
+  # fig.height = 7,
   fig.width = 6,
   fig.asp = 0.618,  # 1 / phi
   fig.show = "hold",
-  #,out.width = "100%"
+  out.width = "100%",
   dev.args = list(png = list(type = "cairo-png")),
   optipng = "-o1 -quiet",
-  widgetframe_widgets_dir = 'widgets'
+  widgetframe_widgets_dir = "widgets",
+  screenshot.opt = list(delay = 0.3)
 )
 
 knitr::knit_hooks$set(crop = knitr::hook_pdfcrop)
@@ -28,3 +30,15 @@ if(!knitr:::is_html_output()){
 
 set.seed(2020-05-06)
 options(digits = 3)
+
+view_map = function(x, name){
+  if (knitr::is_latex_output()){
+    tf = tempfile(fileext = ".html")
+    tmap_save(x, tf)
+    webshot2::webshot(tf, file = paste0("widgets/", name, ".png"))
+    knitr::include_graphics(paste0("widgets/", name, ".png"))
+  } else if (knitr::is_html_output()){
+    widgetframe::frameWidget(tmap::tmap_leaflet(x))
+  }
+}
+
